18fd1ce5526b5d104d24033410ddd3ddce78329a
goldnsito <138344266+goldnsito@users.noreply.github.com>
2023-10-22 09:02:45 +0200

    Create passwordprompter.sh

---
+++ passwordprompter.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+function passwd-check() {
+        if [[ "$passwd" == "$passwd2" ]]; then
+                return 0
+        else
+                return 1
+        fi
+}
+
+
+function newcomer() {
+    echo -e "Por favor escriba su nombre de usuario"
+    read -p ">" user
+
+    while true; do
+        unset passwd
+        unset passwd2
+        echo -n "Introduzca su contraseña: "
+        stty -echo
+        PROMPT=""
+        CHARCOUNT=0
+            while IFS= read -p "$PROMPT" -r -s -n 1 CHAR
+            do
+                if [[ $CHAR == $'\0' ]] ; then
+                    break
+                fi
+                if [[ $CHAR == $'\177' ]] ; then
+                    if [ $CHARCOUNT -gt 0 ] ; then
+                        CHARCOUNT=$((CHARCOUNT-1))
+                        PROMPT=$'\b \b'
+                        passwd="${passwd%?}"
+                    else
+                        PROMPT=''
+                    fi
+                elif [[ $CHAR == $'\n' ]] ; then
+                    break
+                else
+                    CHARCOUNT=$((CHARCOUNT+1))
+                    PROMPT='*'
+                    passwd+="$CHAR"
+                fi
+            done
+            echo
+            echo -n "Vuelve a introducir la conrtaseña: "
+            PROMPT=""
+            while IFS= read -p "$PROMPT" -r -s -n 1 CHAR
+            do
+                if [[ $CHAR == $'\0' ]] ; then
+                    CHARCOUNT=0
+                    break
+                fi
+                if [[ $CHAR == $'\177' ]] ; then
+                    if [ $CHARCOUNT -gt 0 ] ; then
+                        CHARCOUNT=$((CHARCOUNT-1))
+                        PROMPT=$'\b \b'
+                        passwd2="${passwd2%?}"
+                    else
+                        PROMPT=''
+                    fi
+                else
+                    CHARCOUNT=$((CHARCOUNT+1))
+                    PROMPT='*'
+                    passwd2+="$CHAR"
+                fi
+            done
+        passwd-check
+        if [ $? -eq 0 ]; then
+            stty echo
+            echo
+            break
+        else
+            echo
+            echo "La contraseña que has escrito no es correcta, por favor escribela de nuevo."
+        fi
+    done
+}
