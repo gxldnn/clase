0ada03114ff274d121ac720a7d7c129a173e568d
goldnsito <138344266+goldnsito@users.noreply.github.com>
2024-02-11 18:39:49 +0100

    asd

--- Azules/Reto1/passwordprompter.sh
+++ Azules/Reto1/passwordprompter.sh
@@ -76,4 +76,3 @@ function newcomer() {
         fi
     done
 }
-newcomer
---
+++ Azules/Reto1/vpnmake.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Script bien bellako made by goldn n moska
+
+##################################
+##
+##      COLORS && MoRE
+##
+
+LOGFILE=$(pwd)/oneshotlog/logfile.log
+ERRFILE=$(pwd)/oneshotlog/errfile.err
+mkdir -p $(pwd)/oneshotlog
+touch $LOGFILE
+touch $ERRFILE
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+GREY="\e[37m"
+RESET="\e[0m"
+
+
+TICK="\xE2\x9C\x93"
+CROSS="\xE2\x9C\x98"
+function tick {
+	echo -e "\r [ $GREEN$TICK$RESET ] $1"
+}
+function cross {
+	echo -e "\r [ $RED$CROSS$RESET ] $1"
+}
+
+##################################
+##
+##      MAIN FUNCTIONS
+##
+
+function oneshotttya(){
+    echo -ne "$GREEN     _       ___  __       _ ___ 
+    / ) )\ ) )_  (_ \` )_) / ) )  
+    (_/ (  ( (__ .__) ( ( (_/ ($RESET\n\n"
+}
+
+function oneshotttyx(){
+    echo -ne "$RED     _       ___  __       _ ___ 
+    / ) )\ ) )_  (_ \` )_) / ) )  
+    (_/ (  ( (__ .__) ( ( (_/ ($RESET $GREY[$YELLOW gxldnn $GREY X$MAGENTA MOSKA ]\n\n"
+}
+
+
+function dot_check {
+    echo -n $2
+    dots=0
+
+    while kill -0 $1 2>/dev/null; do
+        if [ $dots -ge 4 ]; then
+            echo -ne "\b\b\b\b\b         \b\b\b\b\b\b\b\b\b\b\b\b"
+            dots=0
+        fi
+        echo -ne  " ."
+        ((dots++))
+        sleep 0.5
+    done
+
+    wait $1
+    direct_check $? "$2"
+}
+
+function direct_check {
+    case $1 in
+        0)
+            printf "\r%-35s%s [ $GREEN$TICK$RESET ] done.\n" "$2" ""
+            ;;
+        *)
+            printf "\r%-35s%s [ $RED$CROSS$RESET ]̉̉\n Check the error at: $ERRFILE " "$2" ""
+            exit
+            ;;
+    esac
+}
+
+clear
+oneshotttyx
+sleep 10 >> $LOGFILE 2>$ERRFILE &
+dot_check $! "Getting a Sleep"
+pacman -Sy >> $LOGFILE 2>$ERRFILE &
+dot_check $! "Pacman update"
+pacman -Ss >> $LOGFILE 2>$ERRFILE &
+dot_check $! "List " 
