0ffcd3cb5cd82945ba06c8b20f4aca19bbcd45f7
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-17 20:50:19 +0100

    asd

--- vpn-nft.sh
+++ vpn-nft.sh
@@ -140,8 +140,8 @@ dot_check $! "Creando directorios de trabajo"
 
 cd /root/easy-rsa/
 ./easyrsa init-pki >> $LOGFILE 2>$ERRFILE &
-echo -e "\\\"set_var EASYRSA_ALGO \\\"ec\\\"" > /root/easy-rsa/pki/vars
-echo -e "\\\"set_var EASYRSA_DIGEST \\\"sha512\\\"" >> /root/easy-rsa/pki/vars
+echo -e "\\\"set_var EASYRSA_ALGO \\\"ec\\\"" > pki/vars
+echo -e "\\\"set_var EASYRSA_DIGEST \\\"sha512\\\"" >> pki/vars
 sleep 0.2 >> $LOGFILE 2>$ERRFILE &
 dot_check $! "Configurando easy-rsa"
 
