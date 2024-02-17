cd0a658c3860f2e166b8a84ef8d73ffa338508f4
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-17 20:07:30 +0100

    asd

--- vpn-nft.sh
+++ vpn-nft.sh
@@ -158,12 +158,9 @@ echo -e \"set_var EASYRSA_DIGEST         \\\"sha512\\\"\" >> /root/easy-rsa/pki/
 echo -e \"Ahora ejecuta el siguiente comando:\n scp /root/easy-rsa/pki/ca.crt root@$vpn_ip:/etc/openvpn/server/\"
 echo -e \"Una vez el ca.crt este en tu VPN COPIALO a \\\"/root/client-configs/keys\\\"\"
 "
+
 echo "$ca_script" > "ca.sh"
 chmod +x ca.sh
 echo "Ejecuta en el ca server el siguiente comando: nc $vpn_ip 9000"
-nc -lvp 9000 -k -e ./ca.sh
+nc -lp 9000 -k -e ./ca.sh
 
-#ejecucion de script reomoto desde la vpn hacia el ca
-#nc -lvp -k -e ./scriptca.sh
-#en el ca
-#nc $vpn_ip (puerto)
