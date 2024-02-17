7d871af283b8df0a3d04bf253fd6c49893a8205b
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-17 20:59:54 +0100

    asd

--- vpn-nft.sh
+++ vpn-nft.sh
@@ -171,8 +171,9 @@ echo -e \"Una vez el ca.crt este en tu VPN COPIALO a \\\"/root/client-configs/ke
 
 echo "$ca_script" > "ca.sh"
 chmod +x ca.sh
-nc -lp 9000 -k -e ./ca.sh >> $LOGFILE 2>$ERRFILE &
-dot_check $! "Ejecuta en el ca server el siguiente comando: nc $vpn_ip 9000" 
 
+nc -lp 9000 -k -e ./ca.sh >> $LOGFILE 2>$ERRFILE &
+dot_check $! "Ejecutando script remoto" 
+echo -n "Ejecuta en el ca server el siguiente comando: nc $vpn_ip 9000"
 
 
