a1c929bb687f23adcc015d5b39235e4735ce3bc6
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-21 23:02:52 +0100

    asd

--- salt-minion.sh
+++ salt-minion.sh
@@ -101,7 +101,7 @@ read -p ">" minion_id
 apt update >> $LOGFILE
 apt install curl -y >> $LOGFILE 2>$ERRFILE &
 dot_check $! "Instalando Curl"
-curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg >> $LOGFILE 2>$ERRFILE &
+curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
 dot_check $! "Descargando Repositorios"
 echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/12/amd64/latest bookworm main" > /etc/apt/sources.list.d/salt.list
 sleep 1 >> $LOGFILE 2>$ERRFILE &
