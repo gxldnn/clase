bc595af7662cb5929d4c3fb4b41fc1e0dabdebd6
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-22 01:06:34 +0100

    asd

--- salt-minion.sh
+++ salt-minion.sh
@@ -105,7 +105,7 @@ echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64]
 sleep 3 >> $LOGFILE 2>$ERRFILE &
 dot_check $! "Descargando Repositorios"
 sleep 1 >> $LOGFILE 2>$ERRFILE &
-dot_check $! "Añadiendo Repositorios"
+dot_check $! "Añadiendo Repositorios "
 
 apt update >> $LOGFILE 2>$ERRFILE &
 dot_check $! "Actualizando Repositorios"
