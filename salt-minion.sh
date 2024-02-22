7092cf462e875285ff4d017e4388144119a3d91c
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-22 02:12:59 +0100

    as

--- salt-minion.sh
+++ salt-minion.sh
@@ -100,8 +100,7 @@ TITULODEVENTANA="Salt installe by gxldnn"
 TITULODEMENU="Salt-Stack Minion Service"
 MENU="Escoge una de las opciones"
 
-OPTIONS=(1 "Instalar Salt Minion"
-         2 "Instalacion limpia de Salt-Minion (recomendado)")
+OPTIONS=(1 "Instalar Salt Minion")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$TITULODEVENTANA" \
@@ -152,57 +151,5 @@ case $CHOICE in
             echo -e "id: $minion_id" >> /etc/salt/minion
             systemctl restart salt-minion.service >> $LOGFILE 2>$ERRFILE &
             dot_check $! "Configurando Minion"
-            ;;
-        2)
-            clear
-            screen
-            pkill dpkg
-            dpkg --configure -a
-            pkill dpkg
-            apt purge salt-common -y >> $LOGFILE 2>$ERRFILE &
-            pkill dpkg
-            dot_check $! "Borrando Salt"
-                function cleanup() {
-                    rm -r /etc/salt/
-                    rm -r /var/run/salt
-                    rm -r /var/log/salt
-                    rm -r /var/cache/salt
-                    rm -r /etc/apt/sources.list.d/salt.list
-                }
-            cleanup >> /dev/null 2>&1
-            sleep 0.5 >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Limpiando salt-orphans"
-            sleep 2
-            dot_check $! "Entrando al installer"
-
-            clear
-            screen
-            ping -c 1 -W 5 google.com >> $LOGFILE 2>$ERRFILE &
-            test-ping $?
-
-            echo -e "[$YELLOW!$RESET] Ip de el Salt-Master"
-            read -p ">" master_ip
-            echo -e "[$YELLOW!$RESET] Nombre del Minion"
-            read -p ">" minion_id
-
-
-            apt update >> $LOGFILE
-            clear
-            screen
-            apt install curl -y >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Instalando Curl"
-            curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-            echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/12/amd64/latest bookworm main" > /etc/apt/sources.list.d/salt.list
-            sleep 3 >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Descargando Repositorios"
-            apt update >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Actualizando Repositorios"
-            apt install salt-minion -y >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Instalando Salt-Minion"
-
-            echo -e "master: $master_ip" > /etc/salt/minion
-            echo -e "id: $minion_id" >> /etc/salt/minion
-            systemctl restart salt-minion.service >> $LOGFILE 2>$ERRFILE &
-            dot_check $! "Reconfigurando Minion"
             ;;
 esac
