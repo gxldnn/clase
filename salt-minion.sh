aa94cf36fbfba90128d9025ae763e99ddb8582a4
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-02-22 01:38:50 +0100

    sda

--- salt-minion.sh
+++ salt-minion.sh
@@ -105,7 +105,6 @@ CHOICE=$(dialog --clear \
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
-
 clear
 case $CHOICE in
         1)
@@ -183,72 +182,3 @@ case $CHOICE in
             dot_check $! "Reconfigurando Minion"
             ;;
 esac
-
-
-
-
-
-
-
-
-
-
-
-
-
-clear
-screen
-echo -e "[ 1 ] Primera Instalacion de salt"
-echo -e "[ 2 ] Instalacion Limpia de Salt (usar en caso de segunda o tercera instalación)"
-read -p ">" install_status
-function first_install () {
-    if [[ install_status == "1" ]]; then
-        echo -n ""
-    else
-        clear
-        screen
-        apt purge salt-common -y >> $LOGFILE 2>$ERRFILE &
-        dot_check $! "Borrando Salt"
-            function cleanup() {
-                rm -r /etc/salt/
-                rm -r /var/run/salt
-                rm -r /var/log/salt
-                rm -r /var/cache/salt
-            }
-        cleanup > /dev/null 2>&1
-        sleep 0.1 >> $LOGFILE 2>$ERRFILE &
-        dot_check $! "Limpiando salt-orphans"
-        sleep 2
-        dot_check $! "Entrando al installer"
-        
-    fi
-    clear
-    screen
-    ping -c 1 -W 5 google.com >> $LOGFILE 2>$ERRFILE &
-    test-ping $?
-
-    echo -e "[$YELLOW!$RESET] Ip de el Salt-Master"
-    read -p ">" master_ip
-    echo -e "[$YELLOW!$RESET] Nombre del Minion"
-    read -p ">" minion_id
-
-
-    apt update >> $LOGFILE
-    apt install curl -y >> $LOGFILE 2>$ERRFILE &
-    dot_check $! "Instalando Curl"
-    curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/debian/12/amd64/latest bookworm main" > /etc/apt/sources.list.d/salt.list
-    sleep 3 >> $LOGFILE 2>$ERRFILE &
-    dot_check $! "Descargando Repositorios"
-    apt update >> $LOGFILE 2>$ERRFILE &
-    dot_check $! "Actualizando Repositorios"
-    apt install salt-minion -y >> $LOGFILE 2>$ERRFILE &
-    dot_check $! "Instalando Salt-Minion"
-
-    echo -e "master: $master_ip" > /etc/salt/minion
-    echo -e "id: $minion_id" >> /etc/salt/minion
-    systemctl restart salt-minion.service >> $LOGFILE 2>$ERRFILE &
-    dot_check $! "Reconfigurando Minion"
-}
-
-first_install
