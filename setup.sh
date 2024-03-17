2c629ab4302d8347aba98c85500d85fbcb23691f
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-03-17 05:02:29 +0100

    Update and rename setup.py to setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+install_requirements() {
+    local requirements=(
+        "paramiko"
+        "base64"
+        "traceback2"
+        "keyboard"
+        "platformdirs"
+        "rich"
+        "simple-term-menu"
+        "Pillow"
+        "ipaddress"
+    )
+
+    for pkg in "${requirements[@]}"; do
+        if pip install "$pkg"; then
+            echo "Paquete '$pkg' instalado con éxito."
+        else
+            echo "Error al instalar el paquete '$pkg'."
+            exit 1
+        fi
+    done
+}
+
+main() {
+    if ! command -v pip >/dev/null; then
+        echo "Error: pip no está instalado. Asegúrate de tener Python y pip instalados."
+        exit 1
+    fi
+
+    install_requirements
+}
+
+main
