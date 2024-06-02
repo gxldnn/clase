bdfffce458ce6beaadbd97d4da1c8f531828fec8
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-06-03 01:45:54 +0200

    asd

--- xavi/xavi.sh
+++ xavi/xavi.sh
@@ -4,20 +4,44 @@ function pdispcat(){
     cat programas.txt | grep "@" | awk '{print $2, $3}'
 
 }
+
+
+
 function lavado(){
    clear
    echo -e "Que progarma quieres ejecutar?"
    read -p ">" programa
    case programa in
-        0)
-        pdispcat
-        echo -e "Presiona [Enter para salir]:"
-        read a
-        break
+        0|[Ss][Uu][Aa][Vv][Ee])
+        echo "### OK ejecutando ###"
+        cat programas.txt | grep
         ;;
-        1)
-        lavado
+        1|[Ii][Nn][Tt][Ee][Nn][Ss][Oo])
+        ;;
+        2|[Ll][Aa][Nn][Aa])
+        ;;
+        *)
+        echo -e "El programa no existe: Presiona Enter para salir..."
+
     esac
+}
+
+extract_program() {
+  local program_name="$1"
+  awk -v program="$program_name" '
+  BEGIN { print_program = 0 }
+  {
+    if ($0 ~ "^@ " program) {
+      print_program = 1
+      next
+    } 
+    if ($0 ~ "^@ " && print_program) {
+      print_program = 0
+    }
+    if (print_program) {
+      print
+    }
+  }' "$FILE"
 }
 
 #function atiempo(){
@@ -40,6 +64,11 @@ while true; do
         ;;
         1)
         lavado
+        ;;
+        2)
+        extract_program "Programa suave""
+        ;;
+
     esac
 done
 
