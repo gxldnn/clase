8c695c6c373c2229964eae145c18f1090d802187
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-06-03 01:52:37 +0200

    asd

--- xavi/xavi.sh
+++ xavi/xavi.sh
@@ -16,36 +16,34 @@ function pdispcat(){
 
 read -p "Select:" selections
 
-while true; do
-    case selection in
-        0)
-        pdispcat
-        echo -e "Presiona [Enter para salir]:"
-        read a
-        break
-        ;;
-        1)
-            clear
-            echo -e "Que progarma quieres ejecutar?"
-            read -p ">" programa
-            case programa in
-                 0|[Ss][Uu][Aa][Vv][Ee])
-                 echo "### OK ejecutando ###"
-                 cat programas.txt | awk 'NR==1' | awk '{print $1}'
-                 ;;
-                 1|[Ii][Nn][Tt][Ee][Nn][Ss][Oo])j
-                 ;;
-                 2|[Ll][Aa][Nn][Aa])
-                 ;;
-                 *)
-                 echo -e "El programa no existe: Presiona Enter para salir..."
 
-             esac
-        break
-        ;;
-        2)
-        ;;
+case selection in
+    0)
+    pdispcat
+    echo -e "Presiona [Enter para salir]:"
+    read a
+    break
+    ;;
+    1)
+        clear
+        echo -e "Que progarma quieres ejecutar?"
+        read -p ">" programa
+        case programa in
+             0|[Ss][Uu][Aa][Vv][Ee])
+             echo "### OK ejecutando ###"
+             cat programas.txt | awk 'NR==1' | awk '{print $1}'
+             ;;
+             1|[Ii][Nn][Tt][Ee][Nn][Ss][Oo])j
+             ;;
+             2|[Ll][Aa][Nn][Aa])
+             ;;
+             *)
+             echo -e "El programa no existe: Presiona Enter para salir..."
+         esac
+    break
+    ;;
+    2)
+    ;;
+esac
 
-    esac
-done
 
